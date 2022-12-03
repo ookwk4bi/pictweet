e24deafa6fb57c94e8919c78f6680cac719023f9
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-12-03 22:19:36 +0900

    削除機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,6 +16,13 @@ class TweetsController < ApplicationController
     end
   end
 
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    if @tweet.destroy
+      redirect_to root_path
+    end
+  end
+
 private
   def tweet_params
     params.require(:tweet).permit(:name, :text, :image)
