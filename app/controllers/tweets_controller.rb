c26de8722d381e42d6caa5e1dc0c83ef20458614
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-12-03 23:41:54 +0900

    ユーザー機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
   def index
     @tweets = Tweet.all
   end
@@ -45,5 +46,11 @@ private
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
